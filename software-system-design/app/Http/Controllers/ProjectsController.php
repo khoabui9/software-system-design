44c6fb90652f8fbe581ff6dae6d7728d4159105f
tommyck0524 <keung.tommy.ho@gmail.com>
2017-04-06 21:39:32 +0200

    Update ProjectsController.php

--- software-system-design/app/Http/Controllers/ProjectsController.php
+++ software-system-design/app/Http/Controllers/ProjectsController.php
@@ -10,6 +10,29 @@ use Session;
 
 class ProjectsController extends Controller
 {
+      public function showOne($id) {
+        $project = Project::findOrFail($id);
+        $users = DB::table('user_project')
+        ->where('project_id', '=', $id)
+        ->join('users', 'users.id', '=', 'user_id')
+        ->select('users.*')
+        ->get();
+        return view('show.project')->withProject($project)
+        ->with('users', $users);
+    }
+        public function sort() {
+        $sortby = Input::get('sortby');
+        if ($sortby == 'date')
+            $projects =  DB::table('projects')->orderBy('created_at','desc')->paginate(9);
+        else if ($sortby == 'name')
+            $projects = DB::table('projects')->orderBy('name','asc')->paginate(9);
+        else if ($sortby == 'default')
+            $projects =  DB::table('projects')->paginate(9);
+
+        return view('dashboard.projects')->with('projects', $projects);
+        //return response(view('dashboard.projects',array('projects'=>$projects)),200, ['Content-Type' => 'application/json']);
+        }
+
         public function showEdit($id) {
         $project = Project::findOrFail($id);
         $users = DB::table('user_project')
@@ -20,8 +43,19 @@ class ProjectsController extends Controller
         return view('show.projectEdit')->withProject($project)
         ->with('users', $users);
     }
+         public function show(){
+           $projects =  DB::table('projects')->paginate(9);
+           return view('dashboard.projects')->with('projects', $projects);
+    }
+        public function delete($id) {
+        $p = Project::findOrFail($id);
 
-    public function create(Request $request) {
+        $p->delete();
+
+        return redirect()->action('ProjectsController@show');
+    }
+        
+        public function create(Request $request) {
         $this->validate($request, [
             'name' => 'required|unique:projects',
             'description' => 'required'
@@ -34,44 +68,8 @@ class ProjectsController extends Controller
         Session::flash('flash_message', 'Task successfully added!');
         return redirect()->action('ProjectsController@show');
     }
-    
-      public function show(){
-            $projects =  DB::table('projects')->paginate(9);
-            return view('dashboard.projects')->with('projects', $projects);
-    }
-        public function delete($id) {
-        $p = Project::findOrFail($id);
-
-        $p->delete();
-
-        return redirect()->action('ProjectsController@show');
-    }
-    
-        public function sort() {
-        $sortby = Input::get('sortby');
-        if ($sortby == 'date')
-            $projects =  DB::table('projects')->orderBy('created_at','desc')->paginate(9);
-        else if ($sortby == 'name')
-            $projects = DB::table('projects')->orderBy('name','asc')->paginate(9);
-        else if ($sortby == 'default')
-            $projects =  DB::table('projects')->paginate(9);
-
-        return view('dashboard.projects')->with('projects', $projects);
-        //return response(view('dashboard.projects',array('projects'=>$projects)),200, ['Content-Type' => 'application/json']);
-    }
-    
-    public function showOne($id) {
-        $project = Project::findOrFail($id);
-        $users = DB::table('user_project')
-        ->where('project_id', '=', $id)
-        ->join('users', 'users.id', '=', 'user_id')
-        ->select('users.*')
-        ->get();
-        return view('show.project')->withProject($project)
-        ->with('users', $users);
-    }
-    
-        public function update(Request $request, $id) {
+        
+          public function update(Request $request, $id) {
             $p = Project::findOrFail($id);
             $input = $request->all();
             $p->fill($input)->save();
