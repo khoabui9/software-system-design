da0f403f10ff0e3e3bd9bdec72b1d147893f06cb
tommyck0524 <keung.tommy.ho@gmail.com>
2017-05-20 13:11:18 -0400

    add about page

---
+++ software-system-design/resources/views/show/about.blade.php
@@ -0,0 +1,86 @@
+@extends('layouts.app')
+@section('content')
+     <!-- Page Content -->
+      <div class="col-sm-12 pic-head">
+    </div>
+    <div class="container">
+   
+        <!-- Introduction Row -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">About
+                </h1>
+                <p>JustCollab a work tracking and project management application for teams and individuals.<br>
+                <br>
+The application allows users to keep all projects, tasks and files all in one place and easily collaborate with as a team. Moreover, it also contain functions like schedules and group chat which will help organize your work, stick with the schedule and improve work efficiency.
+<br>
+<br>
+It will allow users to create, delete and edit projects or tasks. It can also assign tasks with due dates to members in project group. In addition, the application is planned to have a group chat function.
+</p>
+            </div>
+        </div>
+
+        <!-- Team Members Row -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="page-header">Our Team</h2>
+            </div>
+            <div class="col-lg-4 col-sm-6 text-center">
+                <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
+                <h3>Gia Khoa Bui
+                   
+                </h3>
+                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+            </div>
+            <div class="col-lg-4 col-sm-6 text-center">
+                <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
+                <h3>Jiří Růžička
+                    
+                </h3>
+                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+            </div>
+            <div class="col-lg-4 col-sm-6 text-center">
+                <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
+                <h3>Jillian O’Neill
+                   
+                </h3>
+                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+            </div>
+            <div class="col-lg-4 col-sm-6 text-center">
+                <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
+                <h3>Chun Tat Kwan
+                   
+                </h3>
+                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+            </div>
+            <div class="col-lg-4 col-sm-6 text-center">
+                <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
+                <h3>Jacob Montgomery
+                    
+                </h3>
+                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+            </div>
+            <div class="col-lg-4 col-sm-6 text-center">
+                <img class="img-circle img-responsive img-center" src="http://placehold.it/200x200" alt="">
+                <h3>Chi Keung Ho
+                </h3>
+                <p>What does this team member to? Keep it short! This is also a great spot for social links!</p>
+            </div>
+        </div>
+
+        <hr>
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; JustCollab 2017</p>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+        </footer>
+
+    </div>
+<!-- /.container -->
+@stop
