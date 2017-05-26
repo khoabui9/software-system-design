6f0874162909ec19414b2f8fabcf50785b27c548
khoabui9 <khoabui.vn@gmail.com>
2017-05-26 04:30:00 -0400

    reduce size

--- software-system-design/resources/views/layouts/show.blade.php
+++ software-system-design/resources/views/layouts/show.blade.php
@@ -5,10 +5,10 @@
     <meta name="author" content="">
     <meta name="csrf-token" content="{{csrf_token() }}">
     <link rel="icon" href="../../favicon.ico">
-      <link rel="stylesheet" type="text/css" href="{{asset('fullcalendar/dist/fullcalendar.css')}}"/>
+      <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.css"/>
     <script src="{{asset('jquery/dist/jquery.min.js')}}"></script>
        <script src="{{asset('moment/moment.js')}}"></script>
-       <script src="{{asset('fullcalendar/dist/fullcalendar.js')}}"></script>
+       <script src="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js"></script>
     
 <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
